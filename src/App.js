bbdcb04e22ef10ba3e1349ae5d8472a041771877
“Paul <paulpelumi@gmail.com>
2023-03-20 12:30:49 +0100

    initial

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import {useQuery} from 'react-scripts';
+import './App.css';
+import axios from "axios";
+
+// This is a typical component with an Asynchronus data fetch 
+// We are using Axios here, but can use standard js fetch command.
+
+function App() {
+  const fetch =  axios("http://swapi.dev/api/planets/");
+  
+  const { isLoading, error, data } = useQuery('swApi', () => fetch );
+  
+  // if (data) console.log (data);
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message;
+  return (
+    <div className="App">
+      <h1>Starwars Planets</h1>
+      
+        <div>
+           {(JSON.stringify(data) !== '{}') && data.data.results.map((planet,index) =><p key={index}>{planet.name}</p>)}           
+        </div>
+
+    </div>
+  );
+}
+export default App;
